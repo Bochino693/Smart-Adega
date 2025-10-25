8797ab3bfe2cf7710ae4b9d9f42ba9fb9c3856d8
Bochino693 <thiago.bochino2001@gmail.com>
2025-10-25 19:28:14 -0300

    Att

--- adega/settings.py
+++ adega/settings.py
@@ -117,7 +117,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'chave-secreta-segura')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
