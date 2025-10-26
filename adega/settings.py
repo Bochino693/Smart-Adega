a5cc75b1ab5178f32d9788f4d4592298d930f3b4
Bochino693 <thiago.bochino2001@gmail.com>
2025-10-26 13:38:53 -0300

    new

--- adega/settings.py
+++ adega/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'chave-secreta-segura')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['smart-adega-amto.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['smart-adega-amto.onrender.com', 'adega-mufasa.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
